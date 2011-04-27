42579281dcf0b36235385c4818491ac53231f488
lsb <leebutterman@gmail.com>
2011-04-27 07:07:27 -0400

    nokogiri makes cleaner code

--- parse-stubs.rb
+++ parse-stubs.rb
@@ -1,43 +1,52 @@
-require 'ipaddr' ; require 'date' ; require 'digest/md5'
-HundredBillion = 100_000_000_000
+# Rip the XML apart at the seams of pages, ensure the title's prefix is good, and then use Nokogiri's DOM parsing to have something more intelligible than regexps.
+# Ideally, we'd pull out the metadata as we run the producer, but stripping all this out with regexps would be heinous, and the full pages are too big for DOM parsing.
 
-IO.popen("sort -u > users#{ARGV[0]}.tsv",'w') {|uf| # id, name
-  File.open("pages#{ARGV[0]}.tsv",'w') {|pf| # id, title
-    File.open("revisions#{ARGV[0]}.tsv",'w') {|rf| # id, user_id, page_id, epochtime
+require 'rubygems'
+require 'nokogiri'
+require 'ipaddr'
+require 'date'
+require 'digest/md5'
+require 'english'
+
+Billion = 1_000_000_000
+$OFS = '|'
+
+IO.popen('sort -u > users.tsv','w') {|uf| # id, name
+  File.open('pages.tsv','w') {|pf| # id, title
+    File.open('revisions.tsv','w') {|rf| # id, user_id, page_id, epochtime
       STDIN.each('</page>') {|page|
-        page_title = page[/<title>(M[^<]*)<\/title>/,1]
-	next unless page_title
-        page_id = page[/<id>(\d+)<\/id>/,1]
-	next unless page_id
-        pf.print("#{page_id}\t#{page_title}\n")
+        next unless page =~ /<title>M/
 
-        page.scan(/<revision>.+?<\/revision>/m) {|rev|
-          rev_id = rev[/<id>(\d+)<\/id>/,1]
-          rev_time = rev[/<timestamp>(.+)<\/timestamp>/,1]
+        nokopage = Nokogiri::XML(page)
+
+        page_id = nokopage.at('page > id').text
+        page_title = nokopage.at('page > title').text
+        pf.print(page_id, page_title << "\n")
+
+        nokopage.css('revision').each {|rev|
+          rev_id = rev.at('id').text
+          rev_time = rev.at('timestamp').text
           rev_epochtime = DateTime.parse(rev_time).strftime('%s')
 
-          user_id = nil
+          user = rev.at('contributor')
           user_name = nil
+          user_id = nil
 
-          if rev[/<contributor deleted="deleted"/]
-	    user_name = "deleted/deleted/vandal"
-	    user_id = -1
-	  else
-	    user = rev[/<contributor>(.+?)<\/contributor>/m,1]
-	    username = user[/<username>(.+?)<\/username>/,1]
-	    if username
-	      user_name = username
-	      user_id = user[/<id>(\d+)<\/id>/,1]
-	    else
-	      user_name = user[/<ip>(.+)<\/ip>/,1] || ''
-              user_id = (HundredBillion+IPAddr.new(user_name).to_i) rescue (- Digest::MD5.hexdigest(user_name)[0,16].to_i(16))
+          if rev.at('contributor')['deleted']
+            user_name = "vandalism/deleted/deleted"
+            user_id = -1
+          else
+            maybe_username = user.at('username')
+            if maybe_username
+              user_id = user.at('id').text
+              user_name = maybe_username.text
+            else
+              user_name = user.at('ip').text
+              user_id = (Billion + IPAddr.new(user_name).to_i) rescue (-Billion - Digest::MD5.hexdigest(user_name)[0,15].to_i(16))
             end
           end
-
-          throw "no contributor: #{rev.text}" unless user_id
-
-          uf.print("#{user_id}\t#{user_name}\n")
-          rf.print("#{rev_id}\t#{user_id}\t#{page_id}\t#{rev_epochtime}\n")
+          uf.print(user_id, user_name << "\n")
+          rf.print(rev_id, user_id, page_id, rev_epochtime << "\n")
         }
       }
     }
