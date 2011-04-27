a3c95d1f89391a1db893f76a84a806382c56d9e0
lsb <leebutterman@gmail.com>
2011-04-27 07:38:10 -0400

    add text size into the consumer; english dependency fails weirdly on REE

--- mh-consumer.rb
+++ mh-consumer.rb
@@ -2,9 +2,8 @@ require 'rubygems'
 require 'lzma'
 require 'json'
 require 'digest/md5'
-require 'english'
 
-$OFS = "|"
+$, = "|"
 
 STDIN.each {|revision_json|
   r = JSON.parse(revision_json)
@@ -12,6 +11,6 @@ STDIN.each {|revision_json|
 
   size_difference = LZMA.compress(r['window'] + r['text']).size - LZMA.compress(r['window']).size
 
-  STDOUT.print(r['new_id'], r['old_id'], size_difference, r['digest'] << "\n")
+  STDOUT.print(r['new_id'], r['old_id'], size_difference, r['text'].size, r['digest'] << "\n")
 }
 
