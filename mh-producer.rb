2ea063aa6069c69d2b0a1cfe9427c3ef8b675650
lsb <leebutterman@gmail.com>
2011-04-27 07:17:38 -0400

    simpler regexping, and no newline suppression because of json

--- mh-producer.rb
+++ mh-producer.rb
@@ -1,3 +1,12 @@
+# Regexps, the Krav Maga of XML parsing.  There's 3TB of XML in the pages-meta-history, so DOM parsing is out, and all of the interesting data is in text nodes, not attributes,
+# so for a SAX parser to discern whether an <id>12345</id> is a page/revision/contributor id requires a state machine, and that's an enormous amount of complexity.  So regexps.
+
+# There's a consistent XML node ordering, <page><title>abc</title><revision>...</revision><revision>...</revision></page>, with consecutive revisions,
+# so split by </revision>, process each one in turn, and a <title> signals a new page, and you can run with only one rev (plus a sliding text window) in memory.
+# Note that we lose all of the interesting metadata this way, which we'll read from the stub-meta-history (pages-meta-history minus the text), small enough to afford DOM parsing.
+
+# JSON makes a convenient way to serialize data from producers to consumers with encoding of characters like linebreaks, so split can easily churn through it.
+
 require 'rubygems'
 require 'json'
 require 'digest/md5'
@@ -10,15 +19,15 @@ window_size = 7
 
 STDIN.each('</revision>') {|r|
   maybe_new_title = r[%r%<title>(.+)</title>%,1]
-  worth_processing = (maybe_new_title[/^M/] && !maybe_new_title.include?(':')) if maybe_new_title
+  worth_processing = (maybe_new_title =~ /^M/ && !maybe_new_title.include?(':')) if maybe_new_title
   next unless worth_processing
 
   revision = r[%r%<revision[^>]*>(.+)</revision>%m,1]
   next unless revision
 
-  (sliding_window = [empty_revision] * window_size) if maybe_new_title
+  sliding_window = [empty_revision] * window_size if maybe_new_title
   rid = revision[%r%<id>(\d+)</id>%,1]
-  rtext = revision[%r%<text[^>]*>(.+)</text>%m,1].tr("\n"," ") rescue nil
+  rtext = revision[%r%<text[^>]*>(.+)</text>%m,1]
   next unless rtext
 
   digest = Digest::MD5.hexdigest(rtext)
