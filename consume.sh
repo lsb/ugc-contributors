275f31167a1a12c9d2f4f8ce431030654202e415
lsb <lee@nodictionaries.com>
2012-09-27 18:43:56 +0000

    no longer necessary to separate parsing pages-meta-history XML and computing diffs; P-M-H is sharded to 170 files

--- consume.sh
+++
@@ -1,1 +0,0 @@
-/opt/ruby-enterprise-1.8.7-2011.03/bin/ruby mh-diff-consumer.rb < $1 > $1.out
--- produce.sh
+++
@@ -1,1 +0,0 @@
-7z e -so enwiki-20110317-pages-meta-history$1.xml.7z | /opt/ruby-enterprise-1.8.7-2011.03/bin/ruby mh-diff-producer.rb | split -l 10000 -a 4 - /$1/$1-diffs-
